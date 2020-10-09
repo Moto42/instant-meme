47477125dc3358aed8a948a7e05059e5278e3670
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-10-09 09:16:37 -0500

    testdata added

--- api/memebuidler.test.js
+++ api/memebuidler.test.js
@@ -1,3 +1,20 @@
 const { test } = require("@jest/globals");
+const memebuilder = require('./memebuilder');
+
+const testData = {
+  template: 'testmeme.png',
+  texts: {
+    t1: {
+      text: 'strawberrry',
+      x: 10,
+      y: 20,
+    },
+    t2: {
+      text: 'grapefruit',
+      x: 30,
+      y: 50,
+    }
+  }
+}
 
 test.todo('tests');
