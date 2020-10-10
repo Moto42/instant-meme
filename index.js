79e657c9113afb642377f3893ca9ccfe8bc5dfe0
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-10-10 16:55:41 -0500

    Now returning memes generated

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const requestParser = require('./api/requestParser');
+const memeBuilder = require('./api/memebuilder');
 
 const commonHandlers = {
-  e405: function(req, res, next) {
+  e405: function(_req, res, _next) {
     res.status(405).send('This Api only accepts GET requests.');
   },
+  e404: function (_req, res, _next) {
+    res.status(404).send('File Not Found');
+  }
 };
 
-function memeHandler(req, res, next) {
+async function memeHandler(req, res, _next) {
   const memeName = req.params['memeName'];
   if(!memeName) {
     res.status(400).json({message: 'No meme template specified'});
   }
   else {
-    res.send('');
+    let template;
+    try {
+      template = await requestParser(req);
+    }
+    catch(e) {
+      res.status(404).send(`Could not find a meme named ${memeName}.`);
+      return;
+    }
+    const meme = await memeBuilder(template);
+    res.set('Content-Type', 'image/png')
+    res.send(meme);
   }
 }
 
