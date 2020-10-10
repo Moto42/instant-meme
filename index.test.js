4f0d562170f505a2c20e2e91be5a065ef546d943
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-10-10 16:55:27 -0500

    Regression testing added for memes

--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
 const app = require('./index');
 const {mwsupertest} = require('middleware-supertest');
 const { test, describe, expect } = require('@jest/globals');
-
+const {toMatchImageSnapshot} = require('jest-image-snapshot');
+expect.extend({
+  toMatchImageSnapshot,
+});
 
 
 describe('should only accept GET requests', () => {
@@ -66,4 +69,34 @@ describe('Rejects requests that do not specify a meme template', () => {
     expect(res.status).not.toEqual(400);
     done();
   });
+});
+
+test('404s a missing meme', async (done) => {
+  const res = await mwsupertest(app)
+    .get('/notFound?t1=watermellons')
+    .query({
+      t1: 'watermellons',
+    })
+  expect(res.status).toEqual(404);
+  done();
+});
+describe('regression testing', () => {
+  test('test-meme "cherries"', async (done) => {
+    const res = await mwsupertest(app)
+      .get('/test-meme?t1=cherriess')
+      .query({
+        t1: 'cherriess',
+      });
+    expect(res.body).toMatchImageSnapshot();
+    done();
+  });
+  test('test-meme "strawberry"', async (done) => {
+    const res = await mwsupertest(app)
+      .get('/test-meme?t1=strawberrys')
+      .query({
+        t1: 'strawberrys',
+      });
+    expect(res.body).toMatchImageSnapshot();
+    done();
+  });  
 });
